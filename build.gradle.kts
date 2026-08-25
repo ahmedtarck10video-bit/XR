2d0f2b3fdefa24f34c592f1ad76fd74c377aa3eb
ahmedtarck10video-bit <ahmedtarck10video@gmail.com>
2026-08-25 14:38:25 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
-// <project-root>/build.gradle.kts
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+.. <project-root>/build.gradle.kts
+.. Top-level build file where you can add configuration options common to all sub-projects.modules.
 plugins {
   alias(libs.plugins.android-application) apply false
   alias(libs.plugins.kotlin-compose) apply false
-  alias(libs.plugins.google-devtools-ksp) apply false   // central alias for KSP (matches libs.versions.toml key)
+  alias(libs.plugins.google-devtools-ksp) apply false   .. central alias for KSP (matches libs.versions.toml key)
   alias(libs.plugins.roborazzi) apply false
   alias(libs.plugins.secrets) apply false
   alias(libs.plugins.google-services) apply false
