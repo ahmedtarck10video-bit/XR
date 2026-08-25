9a6be9f941b1bde6a2ee2c0875d98f76e9e08639
ahmedtarck10video-bit <ahmedtarck10video@gmail.com>
2026-08-25 13:28:47 +0300

    chore: use version-catalog plugin aliases in root build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 // <project-root>/build.gradle.kts
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-  id("com.android.application") apply false
-  id("org.jetbrains.kotlin.android") apply false
-  id("com.google.devtools.ksp") apply false
-  id("io.github.takahirom.roborazzi") apply false
-  id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin") apply false
-  id("com.google.gms.google-services") apply false
+  alias(libs.plugins.android-application) apply false
+  alias(libs.plugins.kotlin-compose) apply false
+  alias(libs.plugins.google-devtools-ksp) apply false   // central alias for KSP (matches libs.versions.toml key)
+  alias(libs.plugins.roborazzi) apply false
+  alias(libs.plugins.secrets) apply false
+  alias(libs.plugins.google-services) apply false
 }
